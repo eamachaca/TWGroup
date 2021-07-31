3c3efd1891b702881508add8a0a108ef469dfaa8
vagrant <vagrant@homestead>
2021-07-31 17:44:50 +0000

    Remote previous finish

---
+++ resources/lang/en/challenge.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'id' => 'ID',
+    'actions' => 'Actions',
+    'tasks' => [
+        'name' => 'Task Name',
+        'description' => 'Task Description',
+        'user' => 'User Assigned',
+    ],
+];
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,4 +31,5 @@ Route::get('first', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('tasks', [TaskController::class,'index']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
